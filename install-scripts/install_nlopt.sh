65b232910d7c256f7aaab559142ecd40e8f267c0
ankur-gupta <ankur@perfectlyrandom.org>
2016-11-20 22:15:17 -0800

    Updating NLopt install script

--- install-scripts/install_nlopt.sh
+++ install-scripts/install_nlopt.sh
@@ -5,6 +5,10 @@ cd /tmp
 rm -rf /tmp/nlopt
 git clone git@github.com:stevengj/nlopt.git
 cd /tmp/nlopt
+
+# Install octave dev and swig (for python)
+# sudo apt install liboctave-dev
+# sudo apt-get install swig
 cmake -DCMAKE_INSTALL_PREFIX=~/toolbox/nlopt .
 make
 rm -rf ~/toolbox/nlopt
@@ -15,5 +19,10 @@ rm -rf ~/anaconda/lib/python2.7/site-packages/nlopt
 cp -r ~/toolbox/nlopt/lib/python2.7/site-packages ~/anaconda/lib/python2.7/site-packages/nlopt
 cd ~/anaconda/lib/python2.7/site-packages/nlopt
 mv ~/anaconda/lib/python2.7/site-packages/nlopt/nlopt.py ~/anaconda/lib/python2.7/site-packages/nlopt/__init__.py
-cd ~/toolbox/examples/
-python ~/toolbox/examples/nlopt_example.py
+cd ~/toolbox/numerical-software-examples/examples/nlopt
+
+# May need to install
+# conda install libgcc
+# or, update matplotlib
+# conda update matplotlib
+python ~/toolbox/numerical-software-examples/examples/nlopt/nlopt_example.py
